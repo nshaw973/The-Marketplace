ad00f8f8548d50e657496de9176d353d737f2271
stephen Batelaan <sbatelaan91@gmail.com>
2023-04-10 19:29:15 -0700

    jj

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const searchRoutes = require('./api/search');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 router.use('/api/search', searchRoutes);
 
 
